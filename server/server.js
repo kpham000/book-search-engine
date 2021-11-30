7d2781d4edd1f57714aef5cbda9b41a121abc232
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-11-29 23:28:09 -0800

    needs work

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema');
+const { authMiddleware } = require('./utils/auth');
 // const routes = require('./routes');
 
 const app = express();
@@ -12,9 +13,12 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.applyMiddleware({ app });
+server.start()
+  .then(res => {
+    applyMiddleware({ app });
+})
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -23,6 +27,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   // app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
